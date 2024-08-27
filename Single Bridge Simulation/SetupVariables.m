632e71527c38ec22d96d96a1000bfc83182dae6b
Dopp-IO <42275088+Dopp-IO@users.noreply.github.com>
2024-08-27 16:35:26 -0400

    add working "trapezoidal" sim

---
+++ Single Bridge Simulation/SetupVariables.m
@@ -0,0 +1,12 @@
+polePairs = 6;
+hallOffset = pi/2;
+hvBus = 72;
+switchingFreq = 4000;
+pidP = 0.01;
+pidI = 0.0;
+flipRotorAngle = 1;
+flipCurrent = 1;
+qAmps = 100;
+dAmps = 0;
+minModulation = 0.01;
+maxModulation = 0.99;
