e4e0fa4bb63f969d610c7a35b4cd979c9081f5c3
Dopp-IO <42275088+Dopp-IO@users.noreply.github.com>
2024-09-17 17:00:53 -0400

    stash

---
+++ Nucleo Motor Control/include/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32g4xx_hal.h"
+
+#define LED_PIN                                GPIO_PIN_5
+#define LED_GPIO_PORT                          GPIOA
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+
+#endif // __MAIN_H
--- Nucleo Motor Control/src/main.c
+++ Nucleo Motor Control/src/main.c
@@ -1,7 +1,35 @@
-#include "stm32g4xx_hal.h"
+#include "main.h"
 #include "parameters.h"
+
+
+void LED_Init();
 
 int main()
 {
+    HAL_Init();
+    LED_Init();
+
+    while(1)
+    {
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+        HAL_Delay(1000);
+    };
+
     return 0;
+}
+
+void LED_Init()
+{
+  LED_GPIO_CLK_ENABLE();
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
 }
